55b6f15528198f1e326dfca23fc8c2de5c3d1793
Deivid21 <david.parra.ignacio@gmail.com>
2025-08-07 15:46:45 -0400

    berlna: Switch common name to sm8350
    Change-Id: I8a02dfc326cc64f4855276a4a9c8091b194a28e4
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -22,7 +22,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'vendor/motorola/sm7325-common',
+    'vendor/motorola/sm8350-common',
     'hardware/motorola',
     'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8350',
@@ -68,6 +68,6 @@ module = ExtractUtilsModule(
 
 if __name__ == '__main__':
     utils = ExtractUtils.device_with_common(
-        module, 'sm7325-common', module.vendor
+        module, 'sm8350-common', module.vendor
     )
     utils.run()
